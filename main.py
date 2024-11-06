ba36243566225dece0e8e476de47b125780013b3
Roi-sysmique <elhechmihamza@gamil.com>
2024-11-06 19:43:28 +0100

    game-over-feature-added

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import random
 
 pygame.init()
 
-# Define screen dimensions and square size for grid cells
+# Define screen dimensions and cell size for the grid
 SCREEN_HEIGHT = 450
 SCREEN_WIDTH = 450
-square_length = 30
+CELL_SIZE = 30
 
-# Initialize the game environment with a grid, each cell represented by '.'
-game_grid = [['.' for _ in range(int(SCREEN_WIDTH / square_length))] for _ in
-             range(int((SCREEN_HEIGHT - 60) / square_length))]
+# Initialize the game grid; each cell is represented by '.'
+game_grid = [['.' for _ in range(int(SCREEN_WIDTH / CELL_SIZE))] for _ in
+             range(int((SCREEN_HEIGHT - 60) / CELL_SIZE))]
 
 # Get the number of rows and columns in the grid
 num_rows = len(game_grid)
@@ -18,11 +18,13 @@ num_cols = len(game_grid[0])
 
 # Set up the display
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_WIDTH))
-pygame.display.set_caption('minesweeper')
+pygame.display.set_caption('Minesweeper')
 
 CLOCK = pygame.time.Clock()
 FPS = 120
 mouse_click = None
+
+# Mapping numbers to their corresponding image paths
 num_to_image = {
     1: "Assets/number-1.png",
     2: "Assets/number-2.png",
@@ -33,28 +35,28 @@ num_to_image = {
     7: "Assets/number-7.png",
     8: "Assets/number-8.png"
 }
+game_over = False
 
 
 class Cell(pygame.sprite.Sprite):
     def __init__(self, row, col):
         pygame.sprite.Sprite.__init__(self)
-        self.length = square_length
+        self.size = CELL_SIZE
         self.col = col
         self.row = row
 
         # Create a surface for the cell and define its position
-        self.image = pygame.transform.scale(pygame.image.load('Assets/block.png'), (square_length, square_length))
+        self.image = pygame.transform.scale(pygame.image.load('Assets/block.png'), (CELL_SIZE, CELL_SIZE))
         self.rect = self.image.get_rect()
-        self.rect.topleft = (self.col * square_length, (self.row * square_length) + 60)
+        self.rect.topleft = (self.col * CELL_SIZE, (self.row * CELL_SIZE) + 60)
         self.mark_flag = False
         self.last_click_id = None
         self.revealed = False
-        # Set mine status and color based on game_grid
-        if game_grid[self.row][self.col] == '*':
-            self.mine = True
-        else:
-            self.mine = False
 
+        # Set mine status based on game_grid
+        self.mine = game_grid[self.row][self.col] == '*'
+
+        # Neighboring cell offsets for mine counting
         self.neighbor_offsets = [(-1, 0), (1, 0), (0, -1), (0, 1),
                                  (-1, -1), (-1, 1), (1, -1), (1, 1)]
 
@@ -65,12 +67,11 @@ class Cell(pygame.sprite.Sprite):
         # Remove invalid neighboring offsets
         self.remove_invalid_neighbors()
 
-        # Calculate the number of neighboring mines if not a mine
-        if not self.mine:
-            self.count_mines()
+        # Calculate the number of neighboring mines if cell is not a mine
+        self.count_mines()
 
     def remove_invalid_neighbors(self):
-        # Filter out any offsets that would go out of grid bounds
+        # Filter out offsets that would go out of grid bounds
         self.neighbor_offsets = [(dr, dc) for dr, dc in self.neighbor_offsets
                                  if 0 <= self.row + dr < num_rows and 0 <= self.col + dc < num_cols]
 
@@ -80,42 +81,50 @@ class Cell(pygame.sprite.Sprite):
             row_pos = self.row + dr
             col_pos = self.col + dc
             self.neighbors.append(game_grid[row_pos][col_pos])
-
-        # Count mines among neighbors and update game_grid
-        self.num = self.neighbors.count('*')
-        game_grid[self.row][self.col] = self.num
+        # Calculate number of neighboring mines if not a mine
+        if not self.mine:
+            self.num = self.neighbors.count('*')
+            game_grid[self.row][self.col] = self.num
 
     def update(self, click):
-
-        if click != None and self.rect.collidepoint(click[0]) and click[1] == 2 and click[2] != self.last_click_id and not self.revealed:
+        global game_over
+        if self.revealed and self.num == 0:
+            self.image = pygame.surface.Surface((CELL_SIZE, CELL_SIZE))
+            self.image.fill('grey')
+        elif self.revealed and self.mine:
+            self.image = pygame.transform.scale(pygame.image.load('Assets/clicked_mine.png'),
+                                                (CELL_SIZE, CELL_SIZE))
+        elif self.revealed and not self.mine:
+            self.image = pygame.transform.scale(pygame.image.load(num_to_image[self.neighbors.count('*')]),
+                                                (CELL_SIZE, CELL_SIZE))
+        if click is not None and self.rect.collidepoint(click[0]) and click[1] == 2 and click[2] != self.last_click_id and not self.revealed:
             if not self.mark_flag:
                 self.image = pygame.transform.scale(pygame.image.load("Assets/flag.png"),
-                                                    (square_length, square_length))
+                                                    (CELL_SIZE, CELL_SIZE))
                 self.mark_flag = True
                 self.last_click_id = click[2]
             if self.mark_flag and click[2] != self.last_click_id:
                 self.image = pygame.transform.scale(pygame.image.load('Assets/block.png'),
-                                                    (square_length, square_length))
+                                                    (CELL_SIZE, CELL_SIZE))
                 self.mark_flag = False
                 self.last_click_id = click[2]
-        elif click != None and self.rect.collidepoint(click[0]) and click[1] == 0:
+        elif click is not None and self.rect.collidepoint(click[0]) and click[1] == 0:
             if self.mark_flag:
                 pass
             elif self.num == 0:
-                self.image = pygame.surface.Surface((square_length, square_length))
-                self.image.fill('grey')
                 self.revealed = True
             elif not self.mine:
-                self.image = pygame.transform.scale(pygame.image.load(num_to_image[self.neighbors.count('*')]),
-                                                    (square_length, square_length))
                 self.revealed = True
+            else:
+                self.revealed = True
+                game_over = True
 
 
 def draw_grid():
     # Draw vertical and horizontal lines to create a grid on the screen
-    for x in range(0, int(SCREEN_WIDTH + 1), square_length):
+    for x in range(0, int(SCREEN_WIDTH + 1), CELL_SIZE):
         pygame.draw.line(SCREEN, 'grey', (x, 60), (x, SCREEN_HEIGHT))
-    for y in range(60, int(SCREEN_HEIGHT + 1), square_length):
+    for y in range(60, int(SCREEN_HEIGHT + 1), CELL_SIZE):
         pygame.draw.line(SCREEN, 'grey', (0, y), (SCREEN_WIDTH, y))
 
 
@@ -133,6 +142,29 @@ def place_mines(mines_count):
         game_grid[row][col] = '*'
 
 
+def flood_fill():
+    revealed_cells = []
+    for cell in cells:
+        if cell.num == 0 and cell.revealed:
+            for dr, dc in cell.neighbor_offsets:
+                row_pos = cell.row + dr
+                col_pos = cell.col + dc
+                revealed_cells.append((row_pos, col_pos))
+    for cell in cells:
+        if (cell.row, cell.col) in revealed_cells:
+            if not cell.mark_flag:
+                cell.revealed = True
+
+
+def reveal_all_mines():
+    for cell in cells:
+        if cell.mine and not cell.revealed:
+            cell.image = pygame.transform.scale(pygame.image.load('Assets/mine.png'),
+                                                (CELL_SIZE, CELL_SIZE))
+        if not cell.mine:
+            cell.revealed = True
+
+
 # Place mines and initialize cells
 place_mines(20)
 cells = pygame.sprite.Group()
@@ -148,14 +180,18 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
-        if mouse_key[2]:
-            mouse_click = (pygame.mouse.get_pos(), 2, random.randrange(0, 1000))
-        elif mouse_key[0]:
-            mouse_click = (pygame.mouse.get_pos(), 0, random.randrange(0, 1000))
+        if not game_over:
+            if mouse_key[2]:  # Right click
+                mouse_click = (pygame.mouse.get_pos(), 2, random.randrange(0, 1000))
+            elif mouse_key[0]:  # Left click
+                mouse_click = (pygame.mouse.get_pos(), 0, random.randrange(0, 1000))
 
     # Draw cells and grid
     cells.draw(SCREEN)
     cells.update(mouse_click)
+    flood_fill()
     draw_grid()
+    if game_over:
+        reveal_all_mines()
     pygame.display.update()
     CLOCK.tick(FPS)
